757b296da9a37784286e0d35c4ee63af1f1bc4d6
arzartden <arzartden@yandex.ru>
2022-08-13 23:23:57 +0700

    add import styles and add html static components and add animated burger menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+'use strict';
+import header from './components/header/header.html';
+import main from './components/main/main.html';
+import footer from './components/footer/footer.html';
 import './index.scss';
+
+const wrapper = document.querySelector('.wrapper');
+wrapper.innerHTML = header + main + footer;
+
+const menuBurger = document.querySelector('.header__burger');
+const headerMenu = document.querySelector('.header__menu');
+const body = document.querySelector('body');
+
+const handler = () => {
+  menuBurger.classList.toggle('active');
+  headerMenu.classList.toggle('active');
+  body.classList.toggle('lock');
+};
+
+menuBurger.addEventListener('click', handler);
