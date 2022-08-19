b3fe719c00941f4cc3ad714e521743a156090f47
arzartden <arzartden@yandex.ru>
2022-08-19 12:35:36 +0700

    fix file

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,25 @@
 'use strict';
 
-const menuBurger = document.querySelector('.header__burger');
+const burgerMenu = document.querySelector('.header__burger');
 const headerMenu = document.querySelector('.header__menu');
 const body = document.querySelector('body');
+const headerBefore = document.querySelector('.header', ':before');
 
-const handler = () => {
-  menuBurger.classList.toggle('active');
+const burgerMenuHandler = () => {
+  burgerMenu.classList.toggle('active');
   headerMenu.classList.toggle('active');
   body.classList.toggle('lock');
 };
 
-menuBurger.addEventListener('click', handler);
+const fixingHeader = () => {
+  if (window.pageYOffset > headerBefore.offsetTop) {
+    headerBefore.style.boxShadow = '0px 5px 5px 0px rgba(78,78,78,0.1)';
+    headerBefore.style.transition= 'box-shadow 0.3s ease-in-out';
+  } else {
+    headerBefore.style.boxShadow = 'none';
+  }
+};
+
+burgerMenu.addEventListener('click', burgerMenuHandler);
+
+window.onscroll = fixingHeader;
