96dd5c86c231561ace4bfbf41eaba286905d3b4c
arzartden <arzartden@yandex.ru>
2022-08-20 13:39:25 +0700

    hide secure token

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,4 +1,6 @@
 'use strict';
+import config from '../../../config.json';
+const secureToken = config.secureToken;
 
 /* SmtpJS.com - v3.0.0 */
 //var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
@@ -75,7 +77,7 @@ const contactMeFormCheck = (e) => {
     }
 
     Email.send({
-      SecureToken : "0a23c54e-f3a2-46aa-93f7-6abfedf9b1d8",
+      secureToken,
       To : 'm4stergarden@yandex.ru',
       From : 'arturarzhannikov93@gmail.com',
       Subject : "Contact Me",
