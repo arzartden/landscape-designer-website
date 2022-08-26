fc433b38c7bc5b88cf7cb11def6d8eb51af89570
arzartden <arzartden@yandex.ru>
2022-08-26 23:41:37 +0700

    fix file

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -7,12 +7,13 @@ const To = config.emailTo;
 const From = config.emailFrom;
 
 /* SmtpJS.com - v3.0.0 */
-var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
+//var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
 
 const form = document.querySelector('.contact-me__form');
 const button = document.querySelector('.btn');
-const modal = document.querySelector(".modal");
-const closeButton = document.querySelector(".close-button");
+const modal = document.querySelector('.modal');
+const modalText = document.querySelector('.modal-text');
+const closeButton = document.querySelector('.close-button');
 
 const toggleModal = () => {
   modal.classList.toggle('show-modal');
@@ -70,12 +71,19 @@ form.addEventListener('submit', (e) => {
     SecureToken,
     To,
     From,
-    Subject : "New message from the client",
+    Subject : "НОВОЕ СООБЩЕНИЕ ОТ КЛИЕНТА!",
     Body : data,
   }).then(
   message => {
     if (message === 'OK') {
+      modalText.innerHTML = 'Ваши данные успешно отправлены.'
       modal.classList.toggle("show-modal");
+    } else {
+      modalText.innerHTML = `Ошибка отправки данных. Попробуйте снова <br>
+      или свяжитесь со мной в <a href="https://wa.me/+79339952283" class="whatsapp__link" target="_blank">Whatsapp</a>, 
+      <a href="https://t.me/M4sterGarden" class="telegram__link" target="_blank">Telegram,</a><br>
+      либо просто позвоните <a href="tel:+79339952283" class="tel__link">+7 933 995 22-83</a>.`;
+      modal.classList.toggle("show-modal"); 
     }
   }
   );
