a1d09af45d59378880ddc5eb96b7d2c952824d2b
arzartden <arzartden@yandex.ru>
2022-08-31 23:02:49 +0700

    add dir

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import '/src/styles/index.scss';
+import '/src/components/index/wrapper/wrapper.js';
+import '/src/components/index/header/header.js';
+import '/src/components/index/main/main.js';
+import '/src/components/index/main/_contact-me/contact-me.js';
