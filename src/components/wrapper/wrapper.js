974705c87ab8d9d3c69b25d9ab4fefc85aaf6bc0
arzartden <arzartden@yandex.ru>
2022-08-31 23:05:18 +0700

    rm

--- src/components/wrapper/wrapper.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-import header from '../header/header.html';
-import main from '../main/main.html';
-import footer from '../footer/footer.html';
-
-const wrapper = document.querySelector('.wrapper');
-wrapper.innerHTML = header + main + footer;
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import './index.scss';
-import './components/wrapper/wrapper.js';
-import './components/header/header.js';
-import './components/main/main.js';
-import './components/main/_contact-me/contact-me.js';
