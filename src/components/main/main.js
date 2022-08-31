814eadcbbebca88a55131a9a765ef3951041ab86
arzartden <arzartden@yandex.ru>
2022-08-31 12:07:53 +0700

    fix file

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -6,7 +6,6 @@ import aboutMe from './_about-me/about-me.html';
 import services from './_services/services.html';
 import portfolio from './_portfolio/portfolio.html';
 import contactMe from './_contact-me/contact-me.html';
-// import ('./_contact-me/contact-me.js');
 
 const main = document.querySelector('.main');
 main.innerHTML = intro + social + aboutMe + services + portfolio + contactMe;
